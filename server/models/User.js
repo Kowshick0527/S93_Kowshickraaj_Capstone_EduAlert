addaa6fecdcb85aaa4e23311fed161ffafe381c1
Kowshick <kowshick870@gmail.com>
2026-09-23 09:59:55 +0530

    feat: create mongoose models and schemas

---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["student", "faculty"],
+      required: true,
+    },
+
+    studentId: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
